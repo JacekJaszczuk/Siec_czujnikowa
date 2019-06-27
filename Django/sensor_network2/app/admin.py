d25e07202bf60a28321aefbace9376879777957a
JacekJaszczuk <218320@student.pwr.edu.pl>
2019-06-27 20:53:41 +0200

    Django: dodanie analityka1

--- Django/sensor_network2/app/admin.py
+++ Django/sensor_network2/app/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 from .models import RedisField, Device, Place
 
 # Register your models here.
-admin.site.register(RedisField)
-admin.site.register(Device)
-admin.site.register(Place)
+class RedisFieldAdmin(admin.ModelAdmin):
+    list_display = ["key_name", "description", "device"]
+
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ["name", "description", "place"]
+
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ["name", "description"]
+
+admin.site.register(RedisField, RedisFieldAdmin)
+admin.site.register(Device, DeviceAdmin)
+admin.site.register(Place, PlaceAdmin)
--- Django/sensor_network2/app/models.py
+++ Django/sensor_network2/app/models.py
@@ -7,12 +7,21 @@ class Place(models.Model):
     description = models.CharField(max_length = 200)
     user = models.ManyToManyField(User)
 
+    def __str__(self):
+        return self.name
+
 class Device(models.Model):
     name = models.CharField(max_length = 30)
     description = models.CharField(max_length = 200)
     place = models.ForeignKey(Place, on_delete = models.CASCADE)
 
+    def __str__(self):
+        return self.name
+
 class RedisField(models.Model):
     key_name = models.CharField(max_length = 30)
     description = models.CharField(max_length = 200)
     device = models.ForeignKey(Device, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.key_name
