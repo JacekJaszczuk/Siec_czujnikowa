7af83af9bd8b3a7380e3c8f9b57f608384a4eff2
JacekJaszczuk <218320@student.pwr.edu.pl>
2019-06-27 19:22:57 +0200

    Django: dodanie relacji wiele do wielu

--- Django/sensor_network/aplikacja/admin.py
+++ Django/sensor_network/aplikacja/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Sensor
+from .models import Sensor, Article, Publication, Miejsca
 
 # Register your models here.
 class SensorAdmin(admin.ModelAdmin):
@@ -10,4 +10,13 @@ class SensorAdmin(admin.ModelAdmin):
     ]
     list_display = ("name", "topic", "type")
 
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ["headline"]
+
+class MiejscaAdmin(admin.ModelAdmin):
+    list_display = ["nazwa", "opis"]
+
 admin.site.register(Sensor, SensorAdmin)
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Publication)
+admin.site.register(Miejsca, MiejscaAdmin)
--- Django/sensor_network/aplikacja/models.py
+++ Django/sensor_network/aplikacja/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Sensor(models.Model):
@@ -8,3 +9,32 @@ class Sensor(models.Model):
 
     def __str__(self):
         return self.name
+
+# Relacja wiele do wielu:
+class Publication(models.Model):
+    title = models.CharField(max_length=30)
+
+    # Sortowanie po tytule:
+    class Meta:
+        ordering = ("title",)
+
+    def __str__(self):
+        return self.title
+
+class Article(models.Model):
+    headline = models.CharField(max_length=100)
+    publication = models.ManyToManyField(Publication)
+
+    class Meta:
+        ordering = ("headline",)
+
+    def __str__(self):
+        return self.headline
+
+class Miejsca(models.Model):
+    nazwa = models.CharField(max_length=30)
+    opis = models.CharField(max_length=200)
+    user = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.nazwa
