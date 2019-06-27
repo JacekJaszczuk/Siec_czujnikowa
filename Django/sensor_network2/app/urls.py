7763eac4e302202bd5086b450bdd37891bf89452
JacekJaszczuk <218320@student.pwr.edu.pl>
2019-06-27 23:21:03 +0200

    Django: dodanie reszty widoków

--- Django/sensor_network2/app/urls.py
+++ Django/sensor_network2/app/urls.py
@@ -6,15 +6,7 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('login/', views.login, name="login"),
     path('logout/', views.logout, name="logout"),
-    
-    #path('<int:sensor_id>/', views.detail, name='detail'),
-    #path('<int:sensor_id>/results/', views.results, name='results'),
-    #path('<int:sensor_id>/vote/', views.vote, name='vote'),
-    #path('dodaj/', views.dodaj, name="dodaj"),
-    #path('generyczny/', views.IndexView.as_view(), name='generyczny'),
-    #path('alicja_login/', views.alicja_login, name="alicja_login"),
-    #path('alicja_logout/', views.alicja_logout, name="alicja_logout"),
-    #path('wymaga_logowania/', views.wymaga_logowania, name="wymaga_logowania"),
-    #path('logowanie/', LoginView.as_view(template_name="aplikacja/logowanie.html"), name="logowanie"),
-    
+    path('<int:place_id>/', views.place_detail, name="place_detail"),
+    path('<int:place_id>/<int:device_id>', views.device_detail, name="device_detail"),
+    path('<int:place_id>/<int:device_id>/<int:redisfield_id>', views.redisfield_detail, name="redisfield_detail"),  
 ]
--- Django/sensor_network2/app/views.py
+++ Django/sensor_network2/app/views.py
@@ -4,15 +4,18 @@ from django.urls import reverse
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth import authenticate as auth_authenticate
+from .models import Place, Device
 
 # Create your views here.
 def index(request):
     if request.user.is_authenticated:
         user = request.user.username
+        place_list = request.user.place_set.all()
     else:
         user = "Anonymous"
+        place_list = []
 
-    context = {"user": user}
+    context = {"user": user, "place_list": place_list}
 
     return render(request, "app/index.html", context)
 
@@ -28,3 +31,24 @@ def login(request):
 def logout(request):
     auth_logout(request)
     return HttpResponseRedirect(reverse("app:index"))
+
+def place_detail(request, place_id):
+    user = request.user.username
+    place = Place.objects.get(id=place_id)
+    device_list = place.device_set.all()
+
+    context = {"user": user, "device_list": device_list}
+
+    return render(request, "app/place_detail.html", context)
+
+def device_detail(request, place_id, device_id):
+    user = request.user.username
+    device = Device.objects.get(id=device_id)
+    redisfield_list = device.redisfield_set.all()
+
+    context = {"user": user, "redisfield_list": redisfield_list, "place_id": place_id}
+
+    return render(request, "app/device_detail.html", context)
+
+def redisfield_detail(request, place_id, device_id, redisfield_id):
+    return HttpResponse("To już jest koniec!")
